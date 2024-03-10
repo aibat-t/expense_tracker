c47f02455b7d7215985eec3c861afe74a7509d48
Tozhybayev Aibat <a.tozhybayev@alantech.kz>
2024-03-10 10:10:31 +0500

    Initial commit

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
+
+  final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
+
+  @override
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (contex, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(
+          expense: expenses[index],
+        ),
+      ),
+    );
+  }
+}
